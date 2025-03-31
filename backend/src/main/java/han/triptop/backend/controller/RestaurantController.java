d628f5d77dc63344a7fcde8fc12aeb3ee8732cbc
evaketelaar <ewm.ketelaar@student.han.nl>
2025-03-31 21:54:27 +0200

    controller

---
+++ backend/src/main/java/han/triptop/backend/controller/RestaurantController.java
@@ -0,0 +1,25 @@
+package han.triptop.backend.controller;
+
+import han.triptop.backend.domain.Restaurant;
+import han.triptop.backend.service.RestaurantService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @GetMapping
+    public List<Restaurant> getRestaurants() {
+        return restaurantService.getRestaurants();
+    }
+}
