ee1cc8ad69b413ead78b05f4e84bc5fc4b9ad378
evaketelaar <ewm.ketelaar@student.han.nl>
2025-04-01 23:14:22 +0200

    toString en data aangepast zodat het overeenkomt met wat teamleden ophalen

--- backend/src/main/java/han/triptop/backend/domain/Restaurant.java
+++ backend/src/main/java/han/triptop/backend/domain/Restaurant.java
@@ -1,25 +1,46 @@
 package han.triptop.backend.domain;
 
 public class Restaurant {
+    private String id;
     private String name;
-    private String phonenumber;
-    private String city;
+    private String address;
 
-    public Restaurant(String name, String phonenumber, String city) {
+    public Restaurant(String id, String name, String address) {
+        this.id = id;
         this.name = name;
-        this.phonenumber = phonenumber;
-        this.city = city;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getPhonenumber() {
-        return phonenumber;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
