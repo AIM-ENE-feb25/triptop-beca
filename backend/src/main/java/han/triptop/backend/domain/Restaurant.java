2a8518c52bac44ea095721cfe64800fe07ff2c8a
evaketelaar <ewm.ketelaar@student.han.nl>
2025-04-02 15:32:26 +0200

    Revert "Merge branch 'main' into prototype-atakan"
    This reverts commit 4d8630b6ba1f0e7b4396b62951f1ed4b3660d2fc, reversing
    changes made to 9a0c647cd43ce4f7a87e9f459f3c7a0c0b725cfa.

--- backend/src/main/java/han/triptop/backend/domain/Restaurant.java
+++ backend/src/main/java/han/triptop/backend/domain/Restaurant.java
@@ -1,53 +1,20 @@
 package han.triptop.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @RedisHash("restaurants")
 public class Restaurant implements Serializable {
     @Id
-    private String id;
+    private String uuid;
     private String name;
     private String address;
-
-    public Restaurant(String id, String name, String address) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "Restaurant{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
--- backend/src/main/java/han/triptop/backend/domain/RestaurantDTO.java
+++ backend/src/main/java/han/triptop/backend/domain/RestaurantDTO.java
@@ -1,19 +1,15 @@
 package han.triptop.backend.domain;
 
-public class RestaurantDTO {
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class
+RestaurantDTO {
     private String query;
     private String address;
-
-    public RestaurantDTO(String query, String address) {
-        this.query = query;
-        this.address = address;
-    }
-
-    public String getQuery() {
-        return query;
-    }
-
-    public String getAddress() {
-        return address;
-    }
 }
